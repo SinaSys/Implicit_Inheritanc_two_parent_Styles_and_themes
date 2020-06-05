725cc587863418c91fa0b066dc19f3713858ff50
SinaSystem <sinasystem47@gmail.com>
2020-06-05 12:39:52 +0430

    Initial commit

---
+++ app/src/main/java/com/sriyanksiddhartha/stylesandthemesdemo/MainActivity.java
@@ -0,0 +1,25 @@
+package com.sriyanksiddhartha.stylesandthemesdemo;
+
+import android.graphics.Color;
+import android.graphics.LinearGradient;
+import android.graphics.Shader;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+/**
+ * Author: Sriyank Siddhartha
+ *
+ * Module 3: Styling Views using inheritance
+ *
+ * 		"AFTER" Demo Project
+ * */
+public class MainActivity extends AppCompatActivity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+
+	}
+}
